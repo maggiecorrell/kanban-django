b6f5ef7b2a3658a1cc1959d2c212aed4931564fe
Tara Davis <taradavis@Taras-MacBook-Pro.local>
2016-08-23 14:54:33 -0400

    Made models.

--- kanban_project/not_trello/admin.py
+++ kanban_project/not_trello/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Board, Card, Category
 
-# Register your models here.
+admin.site.register(Board)
+
+admin.site.register(Card)
+
+admin.site.register(Category)
