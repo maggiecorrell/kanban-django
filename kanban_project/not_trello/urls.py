2cf701d1da6bc392604aac6cdf498dc9e9bd54b7
Tara Davis <taradavis@Taras-MacBook-Pro.local>
2016-08-23 17:22:52 -0400

    Condensed login/logout

--- kanban_project/not_trello/urls.py
+++ kanban_project/not_trello/urls.py
@@ -2,18 +2,18 @@ from django.conf.urls import url, include
 from . import views
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/', CreateView.as_view(
-            template_name='register.html',
+            template_name='registration/register.html',
             form_class=UserCreationForm,
             success_url='/'
 
     )),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^login/$', views.LoginView.as_view(), name='login')
-    ]
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/login'})]
 
 # url(r'^/users/', ListCreateAPIView.as_view(queryset=User.objects.all(), serializer_class=UserSerializer), name='user-list')
 # ]
