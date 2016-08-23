4164a7716090069fd748b85391b7fa57b729c283
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-08-23 14:43:46 -0400

    Fixed urls issue

--- kanban_project/kanban_project/urls.py
+++ kanban_project/kanban_project/urls.py
@@ -3,11 +3,11 @@ from rest_framework import routers
 from not_trello import views
 from django.contrib import admin
 
-# router = routers.DefaultRoute()
+router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('not_trello.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
 ]
