e5e23d03c7ca5f3732e58f53443229e03d702320
Tara Davis <taradavis@Taras-MacBook-Pro.local>
2016-08-23 15:44:13 -0400

    setup register url

--- kanban_project/not_trello/urls.py
+++ kanban_project/not_trello/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url, include
 from . import views
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
-]
+    url(r'^$', views.index, name='index'),
+    url(r'^register/', CreateView.as_view(
+            template_name='register.html',
+            form_class=UserCreationForm,
+            success_url='/'
+    )),
+    url(r'^logout/', views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$', views.LoginView.as_view(), name='login')
+    ]
