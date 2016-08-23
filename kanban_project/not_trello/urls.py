a2e57f3291391343ec82f8698d1627af8a090242
Maggie Correll <maggie.correll@gmail.com>
2016-08-23 16:11:45 -0400

    added blocks

--- kanban_project/not_trello/urls.py
+++ kanban_project/not_trello/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
             form_class=UserCreationForm,
             success_url='/'
     )),
-    url(r'^logout/', views.LogoutView.as_view(), name='logout'),
-    url(r'^login/$', views.LoginView.as_view(), name='login')
+    # url(r'^logout/', views.LogoutView.as_view(), name='logout'),
+    # url(r'^login/$', views.LoginView.as_view(), name='login')
     ]
