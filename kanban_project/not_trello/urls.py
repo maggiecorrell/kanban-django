c4b7873231a74c46082883f6ef2322b25662452c
Tara Davis <taradavis@Taras-MacBook-Pro.local>
2016-08-23 18:01:04 -0400

    Setup login and register -- made base file

--- kanban_project/not_trello/urls.py
+++ kanban_project/not_trello/urls.py
@@ -6,12 +6,12 @@ from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^register/', CreateView.as_view(
+    url(r'^register/$', CreateView.as_view(
             template_name='registration/register.html',
             form_class=UserCreationForm,
             success_url='/'
 
-    )),
+    ), name='register'),
     url(r'^login/$', login, name='login'),
     url(r'^logout/$', logout, {'next_page': '/login'})]
 
