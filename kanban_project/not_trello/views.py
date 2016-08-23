3993ee907e2b3cc1212d39ca1fe15e267f2386c3
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-08-23 15:19:45 -0400

    Added viewset

--- kanban_project/not_trello/views.py
+++ kanban_project/not_trello/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Board
+from .serializers import BoardSerializer
+from rest_framework import viewsets
 
 
-def index(self):
-    return HttpResponse('Hello World')
+def index(request):
+    return render(request, 'index.html')
+
+
+class BoardViewSet(viewsets.ModelViewSet):
+    serializer_class = BoardSerializer
+    queryset = Board.objects.all().order_by('name')
