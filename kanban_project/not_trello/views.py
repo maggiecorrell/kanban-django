7dc917b40be34135a08210674db9d36b6202a27b
Tara Davis <tkalindavis@gmail.com>
2016-08-23 16:14:31 -0400

    Updated login and logout features.

--- kanban_project/not_trello/views.py
+++ kanban_project/not_trello/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from .models import Board
 from .serializers import BoardSerializer
 from rest_framework import viewsets
+from django.http import HttpResponse
+from django.views import generic
+from .models import Board, Card, Category
+from django.conf import settings
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.http import HttpResponseRedirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views import View
+from django.views.generic.edit import FormView
 
 
 def index(request):
@@ -11,3 +21,31 @@ def index(request):
 class BoardViewSet(viewsets.ModelViewSet):
     serializer_class = BoardSerializer
     queryset = Board.objects.all().order_by('name')
+
+
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+    def form_valid(self, form):
+        redirect_to = settings.LOGIN_REDIRECT_URL
+        auth_login(self.request, form.get_user())
+        if self.request.session.test_cookie_worked():
+            self.request.session.delete_test_cookie()
+        return HttpResponseRedirect(redirect_to)
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+    @method_decorator(sensitive_post_parameters('password'))
+    def dispatch(self, request, *args, **kwargs):
+        request.session.set_test_cookie()
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+
+class LogoutView(View):
+    template_name = 'logout.html'
+
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return HttpResponseRedirect(settings.LOGOUT_REDIRECT_URL)
