37d35069c76315b27029c82b17859fb81cc5de7b
Tara Davis <tkalindavis@gmail.com>
2016-08-24 17:55:16 -0400

    rearranged models.

--- not_trello/models.py
+++ not_trello/models.py
@@ -5,14 +5,14 @@ from django.db import models
 class Card(models.Model):
     activity = models.CharField(max_length=500)
     description = models.TextField()
-    who_created = models.CharField(max_length=35)
-    when_created = models.DateTimeField(auto_now_add=True)
-    who_updated = models.CharField(max_length=35)
-    when_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=200)
+    # who_created = models.CharField(max_length=35)
+    # when_created = models.DateTimeField(auto_now_add=True)
+    # who_updated = models.CharField(max_length=35)
+    # when_updated = models.DateTimeField(auto_now=True)
 
 
 class Category(models.Model):
-    status = models.CharField(max_length=200)
     cards = models.ForeignKey(Card, on_delete=models.CASCADE)
 
 
