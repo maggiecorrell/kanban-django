314e55d1ade2c906a4b20382f505405b095bfb18
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-08-24 16:57:50 -0400

    Changed urls

--- not_trello/urls.py
+++ not_trello/urls.py
@@ -6,14 +6,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^register/$', CreateView.as_view(
             template_name='registration/register.html',
             form_class=UserCreationForm,
             success_url='/'
 
     ), name='register'),
-    url(r'^login/$', login, name='login'),
+    url(r'^$', login, name='login'),
     url(r'^logout/$', logout, {'next_page': '/login'}, name='logout')
     ]
 
