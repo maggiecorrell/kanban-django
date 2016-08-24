e46b6f2c8ccedfea283d197f52fa579d8b37699d
Tara Davis <tkalindavis@gmail.com>
2016-08-24 17:39:51 -0400

    working on js

--- not_trello/static/js/app.js
+++ not_trello/static/js/app.js
@@ -1,7 +1,54 @@
-var $not_trello = $('#board_name')
+<script>
+var $done = $('#done')
 
-$ajax({
+$.ajax({
   url: '/api/board'
-}).click(function(){
+}).done(function(response){
+  if (response.count > 0) {
+    response.results.forEach(function(result) {
+      // make a table row
+      var $tr = $('<tr>')
 
+      var $name = $('<td>').text(result.name).appendTo($tr)
+
+      $('<td>').text(result.categories).appendTo($tr)
+
+      $name.click(function() {
+        var boardName = $name.text()
+
+        $name.empty()
+
+        var $form = $('<form>').appendTo($name)
+
+        var $input = $('<input type="text">').val(boardName)
+
+        $input.click(function() {
+          return false;
+        })
+
+        $input.appendTo($form)
+
+        $form.submit(function() {
+          var boardName = $input.val()
+
+          $.ajax({
+            method: 'PUT',
+            url: '/api/board/' + result.id + '/',
+            data: {
+              name: boardName,
+              categories: result.categories
+            }
+          })
+
+          $name.empty();
+          $name.text(boardName)
+
+          return false;
+        })
+      })
+
+      $tr.appendTo($board_n)
+    })
+  }
 })
+</script>
