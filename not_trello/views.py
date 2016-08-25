1842c24402ced655f1b1e845f6574d50027163d4
Maggie Correll <maggie.correll@gmail.com>
2016-08-24 21:50:20 -0400

    new stuff

--- not_trello/views.py
+++ not_trello/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Board, Card, Category
 
 
-@login_required
+# @login_required
 def index(request):
     return render(request, 'board.html')
 
