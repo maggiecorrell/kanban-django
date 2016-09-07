51a1f3cc44f3e926e2924f211f4239b9d5789468
tarakdavis <tkalindavis@gmail.com>
2016-09-07 14:23:23 -0400

    Updated to api.. again

--- not_trello/static/js/app.js
+++ not_trello/static/js/app.js
@@ -1,15 +1,6 @@
 var $save = $('#save')
 
-$.ajax({
-  url: '/api/card'
-})
-
-$.ajax({
-  url: '/api/category'
-})
-
-
-$input.appendTo($form)
+// $input.appendTo($form)
 
    $form.submit((function (x) {
     var $board = {
@@ -22,9 +13,9 @@ $input.appendTo($form)
         url: "api/board",
         contentType: "application/json"
     });
-});
+  }));
 
-$input.appendTo($form)
+// $input.appendTo($form)
 
    $form.submit((function (y) {
     var $cat = {
@@ -37,9 +28,9 @@ $input.appendTo($form)
         url: "api/category",
         contentType: "application/json"
     });
-});
+}));
 
-$input.appendTo($form)
+// $input.appendTo($form)
 
    $form.submit((function (z) {
     var $card = {
@@ -51,4 +42,4 @@ $input.appendTo($form)
         url: "api/card",
         contentType: "application/json"
     });
-});
+}));
