9545475ad348ec860a8ea573886e5182b7d65475
tarakdavis <tkalindavis@gmail.com>
2016-09-07 14:15:48 -0400

    Updated api post functions

--- not_trello/static/js/app.js
+++ not_trello/static/js/app.js
@@ -8,14 +8,47 @@ $.ajax({
   url: '/api/category'
 })
 
-$(function (x) {
+
+$input.appendTo($form)
+
+   $form.submit((function (x) {
     var $board = {
       name: board-name,
+      categories_set: categories_set,
     };
     $.ajax({
         type: "POST",
         data :JSON.stringify(board),
         url: "api/board",
+        contentType: "application/json"
+    });
+});
+
+$input.appendTo($form)
+
+   $form.submit((function (y) {
+    var $cat = {
+      cards_set: cards_set,
+      name: cat_name,
+    };
+    $.ajax({
+        type: "POST",
+        data :JSON.stringify(cat),
+        url: "api/category",
+        contentType: "application/json"
+    });
+});
+
+$input.appendTo($form)
+
+   $form.submit((function (z) {
+    var $card = {
+      activity: activity,
+    };
+    $.ajax({
+        type: "POST",
+        data :JSON.stringify(card),
+        url: "api/card",
         contentType: "application/json"
     });
 });
